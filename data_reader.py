9649e9146045ce5012fb8728952dc54a2c99ac16
AmirAli <amirali.kaboli@divar.ir>
2020-07-23 02:49:31 +0430

    add phase2 files

--- data_reader.py
+++ data_reader.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -6,25 +7,81 @@ from bs4 import BeautifulSoup
 from utils import tokenizing
 
 inverted_index = {}
+term_frequency = {}
+doc_frequency = {}
+champion_lists = {}
 
 
-def generate_inverted_index(tokens, doc_id):
-    for token in tokens:
+def generate_inverted_index_and_frequencies(tokens, doc_id):
+    for token in set(tokens):
         if token not in inverted_index:
-            inverted_index[token] = [doc_id]
-        else:
-            inverted_index[token].append(doc_id)
+            inverted_index[token] = []
+        inverted_index[token].append(doc_id)
+
+        if token not in doc_frequency:
+            doc_frequency[token] = 0
+        doc_frequency[token] += 1
+
+    for token in tokens:
+        if token not in term_frequency:
+            term_frequency[token] = {}
+        if doc_id not in term_frequency[token]:
+            term_frequency[token][doc_id] = 0
+        term_frequency[token][doc_id] += 1
+
+
+def vector_normalizing(vec):
+    normalizing_factor = math.sqrt(sum([val ** 2 for val in vec.values()]))
+    return {key: val / normalizing_factor for key, val in vec.items()}
+
+
+def calc_tf_idf(term, doc_id):
+    return (1 + math.log10(term_frequency[term][doc_id])) * math.log10(len(news_details) / doc_frequency[term])
 
 
 if __name__ == '__main__':
-    doc_id = 0
+    news_details = []
     for csv_index in range(6):
-        data = pd.read_csv(f'IR-S19-project-data/ir-news-{2 * csv_index}-{2 * csv_index + 2}.csv')
-        for content in data['content']:
-            text = BeautifulSoup(content).get_text()
-            tokens = list(set(tokenizing(text)))
-            generate_inverted_index(tokens, doc_id)
-            doc_id += 1
+        data = pd.read_csv(f'../IR_project/IR-S19-project-data/ir-news-{2 * csv_index}-{2 * csv_index + 2}.csv')
+        for ind in range(data.shape[0]):
+            news_details.append({
+                'title': data['title'][ind],
+                'content': BeautifulSoup(data['content'][ind]).get_text()
+            })
+
+    champion_list_size = math.floor(math.sqrt(len(news_details)))
+
+    with open('news_details.json', 'w') as json_file:
+        json.dump(news_details, json_file, ensure_ascii=False)
+
+    docs_tokens = []
+    for doc in news_details:
+        docs_tokens.append(tokenizing(doc['content']))
+
+    for doc_id, doc_tokens in enumerate(docs_tokens):
+        generate_inverted_index_and_frequencies(doc_tokens, doc_id)
 
     with open('inverted_index.json', 'w') as json_file:
         json.dump(inverted_index, json_file, ensure_ascii=False)
+    with open('term_frequency.json', 'w') as json_file:
+        json.dump(term_frequency, json_file, ensure_ascii=False)
+    with open('doc_frequency.json', 'w') as json_file:
+        json.dump(doc_frequency, json_file, ensure_ascii=False)
+
+    tf_idf = [{} for _ in range(len(docs_tokens))]
+    for doc_id, doc_tokens in enumerate(docs_tokens):
+        terms = list(set(doc_tokens))
+        for term in terms:
+            tf_idf[doc_id][term] = calc_tf_idf(term, doc_id)
+
+    with open('tf_idf.json', 'w') as json_file:
+        json.dump(tf_idf, json_file, ensure_ascii=False)
+    with open('normalized_tf_idf.json', 'w') as json_file:
+        json.dump([vector_normalizing(doc_tf_idf) for doc_tf_idf in tf_idf], json_file, ensure_ascii=False)
+
+    for term in inverted_index.keys():
+        terms_tf_idf = [(tf_idf[doc_id][term], doc_id) for doc_id in inverted_index[term]]
+        champion_lists[term] = [doc_id for _, doc_id in sorted(terms_tf_idf)[::-1][:champion_list_size]]
+
+    with open('champion_lists.json', 'w') as json_file:
+        json.dump(champion_lists, json_file, ensure_ascii=False)
--- engine.py
+++ engine.py
@@ -1,26 +1,72 @@
+import heapq
 import json
+import math
 
 from utils import tokenizing
 
+result_size = 10
 
-def posting_lists_intersection(posting_lists):
-    if not posting_lists:
-        return 'all tokens are probably in stopwords'
-    result_set = set(posting_lists[0])
-    for posting_list in posting_lists:
-        result_set = result_set.intersection(posting_list)
-    return sorted(list(result_set))
+
+def calc_query_vec(query_tokens):
+    term_frequency = {}
+    for token in query_tokens:
+        if token not in term_frequency:
+            term_frequency[token] = 0
+        term_frequency[token] += 1
+
+    return {
+        term: (1 + math.log10(term_frequency[term])) * math.log10(len(news_details) / doc_frequency[term])
+        for term in set(query_tokens)
+    }
+
+
+def vector_normalizing(vec):
+    normalizing_factor = math.sqrt(sum([val ** 2 for val in vec.values()]))
+    return {key: val / normalizing_factor for key, val in vec.items()}
+
+
+def similarity(doc_vec, query_vec):
+    dot_ans = 0
+    for key in query_vec.keys():
+        if key in doc_vec:
+            dot_ans += doc_vec[key] * query_vec[key]
+    return dot_ans
 
 
 if __name__ == '__main__':
-    with open('inverted_index.json', 'r') as json_file:
+    with open('news_details.json', 'r') as json_file:
+        news_details = json.load(json_file)
+    with open('../IR_project/inverted_index.json', 'r') as json_file:
         inverted_index = json.load(json_file)
+    with open('../IR_project/champion_lists.json', 'r') as json_file:
+        champion_lists = json.load(json_file)
+    with open('../IR_project/doc_frequency.json', 'r') as json_file:
+        doc_frequency = json.load(json_file)
+    with open('../IR_project/normalized_tf_idf.json', 'r') as json_file:
+        normalized_tf_idf = json.load(json_file)
 
     while True:
         query = input()
         query_tokens = tokenizing(query)
 
-        posting_lists = [
-            inverted_index[query_token] if query_token in inverted_index else [] for query_token in query_tokens
+        normalized_query_vec = vector_normalizing(calc_query_vec(query_tokens))
+
+        candidates = []
+        for token in set(query_tokens):
+            # candidates.extend(inverted_index[token])
+            candidates.extend(champion_lists[token])
+        candidates = list(set(candidates))
+
+        candidates_similarity = [
+            (similarity(normalized_tf_idf[doc_id], normalized_query_vec), doc_id)
+            for doc_id in candidates
         ]
-        print(posting_lists_intersection(posting_lists))
+        top_similar_docs = [
+            news_details[candidate_similarity[1]]
+            for candidate_similarity in heapq.nlargest(result_size, candidates_similarity)
+        ]
+
+        for news in top_similar_docs:
+            print(f"title: {news['title']}")
+            print(f"content: {news['content']}")
+            print('#' * 100)
